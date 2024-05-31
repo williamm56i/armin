dc7305d14f4502a573dc55b5e3472e644ca008d1
Tony <wutony791225@hotmail.com>
2024-05-31 14:35:38 +0800

    init

---
+++ src/main/java/com/williamm56i/armin/ArminApplication.java
@@ -0,0 +1,13 @@
+package com.williamm56i.armin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/williamm56i/armin/ArminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamm56i.armin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
